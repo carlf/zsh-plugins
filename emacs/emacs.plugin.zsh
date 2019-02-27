6e4241f155732aca63f2192128f23b87812951d2
Carl Flippin <carlf@carlf.io>
2019-02-26 16:32:34 -0800

    Fix oh-my-zsh-ism

--- emacs/emacs.plugin.zsh
+++ emacs/emacs.plugin.zsh
@@ -10,45 +10,43 @@
 # - Configuration changes made at runtime are applied to all frames.
 
 
-if "$ZSH/tools/require_tool.sh" emacsclient 24 2>/dev/null ; then
-    export EMACS_PLUGIN_LAUNCHER="$ZSH/plugins/emacs/emacsclient.sh"
+export EMACS_PLUGIN_LAUNCHER="$(dirname $0)/emacsclient.sh"
 
-    # set EDITOR if not already defined.
-    export EDITOR="${EDITOR:-${EMACS_PLUGIN_LAUNCHER}}"
+# set EDITOR if not already defined.
+export EDITOR="${EDITOR:-${EMACS_PLUGIN_LAUNCHER}}"
 
-    alias emacs="$EMACS_PLUGIN_LAUNCHER --no-wait"
-    alias e=emacs
-    # open terminal emacsclient
-    alias te="$EMACS_PLUGIN_LAUNCHER -nw"
+alias emacs="$EMACS_PLUGIN_LAUNCHER --no-wait"
+alias e=emacs
+# open terminal emacsclient
+alias te="$EMACS_PLUGIN_LAUNCHER -nw"
 
-    # same than M-x eval but from outside Emacs.
-    alias eeval="$EMACS_PLUGIN_LAUNCHER --eval"
-    # create a new X frame
-    alias eframe='emacsclient --alternate-editor "" --create-frame'
+# same than M-x eval but from outside Emacs.
+alias eeval="$EMACS_PLUGIN_LAUNCHER --eval"
+# create a new X frame
+alias eframe='emacsclient --alternate-editor "" --create-frame'
 
 
-    # Write to standard output the path to the file
-    # opened in the current buffer.
-    function efile {
-        local cmd="(buffer-file-name (window-buffer))"
-        "$EMACS_PLUGIN_LAUNCHER" --eval "$cmd" | tr -d \"
-    }
+# Write to standard output the path to the file
+# opened in the current buffer.
+function efile {
+    local cmd="(buffer-file-name (window-buffer))"
+    "$EMACS_PLUGIN_LAUNCHER" --eval "$cmd" | tr -d \"
+}
 
-    # Write to standard output the directory of the file
-    # opened in the the current buffer
-    function ecd {
-        local cmd="(let ((buf-name (buffer-file-name (window-buffer))))
-                     (if buf-name (file-name-directory buf-name)))"
+# Write to standard output the directory of the file
+# opened in the the current buffer
+function ecd {
+    local cmd="(let ((buf-name (buffer-file-name (window-buffer))))
+    	                   (if buf-name (file-name-directory buf-name)))"
 
-        local dir="$($EMACS_PLUGIN_LAUNCHER --eval $cmd | tr -d \")"
-        if [ -n "$dir" ] ;then
-            echo "$dir"
-        else
-            echo "can not deduce current buffer filename." >/dev/stderr
-            return 1
-        fi
-    }
-fi
+    local dir="$($EMACS_PLUGIN_LAUNCHER --eval $cmd | tr -d \")"
+    if [ -n "$dir" ] ;then
+        echo "$dir"
+    else
+        echo "can not deduce current buffer filename." >/dev/stderr
+        return 1
+    fi
+}
 
 ## Local Variables:
 ## mode: sh
